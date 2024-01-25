81768887d8b76e6b5eff841204635985874a9a05
Ompawaskar <138382841+Ompawaskar@users.noreply.github.com>
2024-01-25 20:59:01 +0530

    init

---
+++ QuitTobacco/src/components/index.js
@@ -0,0 +1,23 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import AuthLayout from './AuthLayout'
+import Login from "./Login";
+import Signup from "./Signup";
+
+export  {
+    Header,
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn,
+    Input,
+    Button,
+    AuthLayout,
+    Login,
+    Signup
+}
